83e3e78d920306ceb8c8d1a65cba46fb89694cf2
JuanMira <juanmira20@hotmail.com>
2021-08-05 10:49:41 -0500

    Changes

--- main.go
+++ main.go
@@ -3,16 +3,18 @@ package main
 import (
 	"net/http"
 	"os"
+
+	"github.com/JuanMira/aws_api/controllers"
 )
 
 
 func main() {
-	http.HandleFunc("/", Greet)
+	http.HandleFunc("/", controllers.Greet)
 	port := os.Getenv("PORT")
 
 	if port == ""{
 		port = "5000"
-		
+
 	}
 	http.ListenAndServe(":"+port, nil)
 }
